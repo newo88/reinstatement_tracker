151de50395a64f772ad4e17fc1f5276c997131b3
Owen McGill <owen.mcgill88@gmail.com>
2021-09-25 19:58:59 +0000

    create function for measures and calcuating the total area for material required

--- run.py
+++ run.py
@@ -14,14 +14,13 @@ SHEET = GSPREAD_CLIENT.open('reinstatement_measure_sheet')
 
 def get_wprn():
 
-    wprn = input("Please enter a WPRN\n") 
+    wprn = input ("Please enter a WPRN\n")
     validate_data(wprn)
-    print(f"you entered {wprn}")
+   
 
 
 def validate_data(wprn):
     try:
-        int(wprn)
         if len(wprn) != 7:
             raise ValueError(
                     f"exactly 7 digits required you entered {len(wprn)}"
@@ -30,5 +29,17 @@ def validate_data(wprn):
         print(f"invalid data {e}") 
 
 
-get_wprn()
+def measures():
 
+
+    length = float(input('Please enter length:\n '))
+    width = float(input('Please enter width:\n '))
+    depth = float(input('Please enter depth:\n '))
+
+    total_area_cubed = (length * width * depth)
+    print('Total Concrete needed')
+    print(total_area_cubed)
+
+
+get_wprn()
+measures()
