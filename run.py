94075895177084d0ea994901fc386f60a1208a28
Owen McGill <owen.mcgill88@gmail.com>
2021-09-27 13:01:57 +0000

    update validation for wprn input to only except a 7 digit number returning an error if anything else is entered

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import gspread
+import numpy as np
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -13,24 +14,27 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('reinstatement_measure_sheet')
 
 def get_wprn():
+    """
+    Request Wprn from user 
+    """
     while True:
         
         wprn = input("Please enter a WPRN\n")
         
-        user_data = wprn.split(",")
-        if validate_data(user_data):
+        
+        if validate_data(wprn):
             print("Wprn is Valid")
             break  
         
-    return user_data    
+    return wprn    
    
 
 def validate_data(values):
     try:
         [int(value) for value in values]
-        if len(values) != 1:
+        if len(values) != 7:
             raise ValueError(
-                    f"exactly 7 digits required you entered {len(wprn)}"
+                    f"exactly 7 digits required you entered {len(values)}"
                 )    
     except ValueError as e:
         print(f"invalid data {e}") 
@@ -41,30 +45,25 @@ def validate_data(values):
    
 
 def measures():
+    while True:
 
+        print("please enter length width and dept followed by a comma,")
+        measure = input("input data here\n")
+         
+        user_measure = measure.split(",")
+        if validate_measures(user_measure):
+            print("measure is Valid")
+            break
+      
+    return user_measure 
+    
 
-    print("please enter length width and dept followed by a comma,")
-    measure = input("input data here\n")
-
-    user_measure = measure.split(",")
-    if validate_data(user_measure):
-        print("measure is Valid")
-          
-        
-    return user_measure    
-
-    #total_area_cubed = (length,  int(width),  int(depth))
-    print('Total Concrete needed')
-    print(total_area_cubed)
-    return total_area_cubed
-
-
-def validate_measures():
+def validate_measures(values):
     try:
-        [int(measure) for measure in measures]
+        [int(value) for value in values]
         if len(values) != 3:
             raise ValueError(
-                    f"exactly 3 digits required you entered {len(wprn)}"
+                    f"exactly 3 digits required you entered {len(values)}"
                 )    
     except ValueError as e:
         print(f"invalid data {e}") 
@@ -73,18 +72,22 @@ def validate_measures():
    
     return True
 
-
-
-
     
 def update_tracker(wprn_data):
     print("updating tracker sheet")
-    total_row = wprn_data + total_area_cubed
+    total_row = wprn_data + user_measure
     tracker_worksheet = SHEET.worksheet('project')
     tracker_worksheet.append_row(total_row)
     print("updated Successfully")
+    print(type(user_measure))
+    print('Total Concrete needed')
+    #total_area_cubed = np.prod(user_measure)
+    #print(total_area_cubed)
+   
+
+    
 
 wprn = get_wprn()
 wprn_data = [int(num) for num in wprn]
-total_area_cubed = measures()
+user_measure = measures()
 update_tracker(wprn_data)
