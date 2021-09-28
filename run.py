6896222f2b3ec2786040bbc8314aef4159037a34
Owen McGill <owen.mcgill88@gmail.com>
2021-09-28 18:53:13 +0000

    code tidy up and doc strings added to functions

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 import gspread
 import numpy as np
+import numpy
+import math
+
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -13,50 +16,70 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('reinstatement_measure_sheet')
 
+
 def get_wprn():
+
+
     """
-    Request Wprn from user 
+    Request Wprn from user
     """
     while True:
-        
+
         wprn = input("Please enter a WPRN\n")
-        
-        
+
+        user_data = wprn.split(",")
         if validate_data(wprn):
             print("Wprn is Valid")
-            break  
-        
-    return wprn    
-   
+            break
+
+    return user_data
+
 
 def validate_data(values):
+
+    """
+    Validates WPRN to make sure it is a
+    seven digit number.
+    """
     try:
         [int(value) for value in values]
-        if len(values) != 7:
+        while len(values) != 7 or (not values.isdigit()):
             raise ValueError(
                     f"exactly 7 digits required you entered {len(values)}"
-                )    
+                )
     except ValueError as e:
-        print(f"invalid data {e}") 
+        print(f"invalid data {e}")
         return False
 
-   
     return True
-   
+
 
 def measures():
+
+    """
+    Requests the dimensions of the hole
+    to be reinstated.
+    """
     while True:
 
         print("please enter length width and dept followed by a comma,")
         measure = input("input data here\n")
-         
+
         user_measure = measure.split(",")
         if validate_measures(user_measure):
             print("measure is Valid")
             break
-      
-    return user_measure 
-    
+    print(type(user_measure))  #list
+    print(type(measure))  #string
+    multiplied_list = [i * 5 for i in user_measure]
+    print(multiplied_list)
+
+    an_array = np.array(user_measure)
+    this_number = an_array * 2
+    print(this_number)
+
+    return user_measure
+
 
 def validate_measures(values):
     try:
@@ -64,30 +87,42 @@ def validate_measures(values):
         if len(values) != 3:
             raise ValueError(
                     f"exactly 3 digits required you entered {len(values)}"
-                )    
+                )
     except ValueError as e:
-        print(f"invalid data {e}") 
+        print(f"invalid data {e}")
         return False
 
-   
     return True
 
-    
+
+def get_area():
+
+    """
+    Takes the measure from measures and calculates
+    the meter cube required for the hole.
+    """
+
+    for i in sorted(user_measure):
+        print(i)
+
+
+
 def update_tracker(wprn_data):
+
+    """
+    Updates the google sheet with the information entered by the user.
+    """
+
     print("updating tracker sheet")
     total_row = wprn_data + user_measure
     tracker_worksheet = SHEET.worksheet('project')
     tracker_worksheet.append_row(total_row)
     print("updated Successfully")
-    print(type(user_measure))
-    print('Total Concrete needed')
-    #total_area_cubed = np.prod(user_measure)
-    #print(total_area_cubed)
-   
+    print(user_measure)
 
-    
 
 wprn = get_wprn()
 wprn_data = [int(num) for num in wprn]
 user_measure = measures()
 update_tracker(wprn_data)
+get_area()
